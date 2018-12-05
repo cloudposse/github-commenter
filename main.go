d722ba42733b4bda3a3bbb1ec540a07fd535d39e
aknysh <andriy.knysh@gmail.com>
2018-12-05 00:26:49 -0500

    Add `delete-comment-regex` argument

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strconv"
 	"text/template"
 )
@@ -25,16 +26,17 @@ func (rt roundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
 }
 
 var (
-	token       = flag.String("token", os.Getenv("GITHUB_TOKEN"), "Github access token")
-	owner       = flag.String("owner", os.Getenv("GITHUB_OWNER"), "Github repository owner")
-	repo        = flag.String("repo", os.Getenv("GITHUB_REPO"), "Github repository name")
-	commentType = flag.String("type", os.Getenv("GITHUB_COMMENT_TYPE"), "Comment type: 'commit', 'pr', 'issue', 'pr-review' or 'pr-file'")
-	sha         = flag.String("sha", os.Getenv("GITHUB_COMMIT_SHA"), "Commit SHA")
-	number      = flag.String("number", os.Getenv("GITHUB_PR_ISSUE_NUMBER"), "Pull Request or Issue number")
-	file        = flag.String("file", os.Getenv("GITHUB_PR_FILE"), "Pull Request File Name")
-	position    = flag.String("position", os.Getenv("GITHUB_PR_FILE_POSITION"), "Position in Pull Request File")
-	format      = flag.String("format", os.Getenv("GITHUB_COMMENT_FORMAT"), "Comment format. Supports 'Go' templates: My comment:<br/>{{.}}")
-	comment     = flag.String("comment", os.Getenv("GITHUB_COMMENT"), "Comment text")
+	token              = flag.String("token", os.Getenv("GITHUB_TOKEN"), "Github access token")
+	owner              = flag.String("owner", os.Getenv("GITHUB_OWNER"), "Github repository owner")
+	repo               = flag.String("repo", os.Getenv("GITHUB_REPO"), "Github repository name")
+	commentType        = flag.String("type", os.Getenv("GITHUB_COMMENT_TYPE"), "Comment type: 'commit', 'pr', 'issue', 'pr-review' or 'pr-file'")
+	sha                = flag.String("sha", os.Getenv("GITHUB_COMMIT_SHA"), "Commit SHA")
+	number             = flag.String("number", os.Getenv("GITHUB_PR_ISSUE_NUMBER"), "Pull Request or Issue number")
+	file               = flag.String("file", os.Getenv("GITHUB_PR_FILE"), "Pull Request File Name")
+	position           = flag.String("position", os.Getenv("GITHUB_PR_FILE_POSITION"), "Position in Pull Request File")
+	format             = flag.String("format", os.Getenv("GITHUB_COMMENT_FORMAT"), "Comment format. Supports 'Go' templates: My comment:<br/>{{.}}")
+	comment            = flag.String("comment", os.Getenv("GITHUB_COMMENT"), "Comment text")
+	deleteCommentRegex = flag.String("delete-comment-regex", os.Getenv("GITHUB_DELETE_COMMENT_REGEX"), "Regex expression to find a comment to delete before creating the new comment")
 )
 
 func getPullRequestOrIssueNumber(str string) (int, error) {
@@ -135,6 +137,8 @@ func main() {
 	http.DefaultClient.Transport = roundTripper{*token}
 	githubClient := github.NewClient(http.DefaultClient)
 
+	// https://developer.github.com/v3/guides/working-with-comments
+	// https://developer.github.com/v3/repos/comments
 	if *commentType == "commit" {
 		if *sha == "" {
 			flag.PrintDefaults()
@@ -151,7 +155,29 @@ func main() {
 			log.Fatal(err)
 		}
 
-		// https://developer.github.com/v3/repos/comments
+		// Find and delete existing comment(s) before creating the new one
+		if *deleteCommentRegex != "" {
+			r, err := regexp.Compile(*deleteCommentRegex)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			listOptions := &github.ListOptions{}
+			comments, _, err := githubClient.Repositories.ListCommitComments(context.Background(), *owner, *repo, *sha, listOptions)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, comment := range comments {
+				if r.MatchString(*comment.Body) {
+					_, err = githubClient.Repositories.DeleteComment(context.Background(), *owner, *repo, *comment.ID)
+					if err != nil {
+						log.Print("Error deleting commit comment", err)
+					}
+				}
+			}
+		}
+
 		commitComment := &github.RepositoryComment{Body: &formattedComment}
 		commitComment, _, err = githubClient.Repositories.CreateComment(context.Background(), *owner, *repo, *sha, commitComment)
 		if err != nil {
@@ -160,6 +186,7 @@ func main() {
 
 		fmt.Println("github-commenter: created GitHub Commit comment", commitComment.ID)
 	} else if *commentType == "pr-review" {
+		// https://developer.github.com/v3/pulls/reviews/#create-a-pull-request-review
 		num, err := getPullRequestOrIssueNumber(*number)
 		if err != nil {
 			log.Fatal(err)
@@ -175,7 +202,6 @@ func main() {
 			log.Fatal(err)
 		}
 
-		// https://developer.github.com/v3/pulls/reviews/#create-a-pull-request-review
 		pullRequestReviewRequest := &github.PullRequestReviewRequest{Body: &formattedComment, Event: github.String("COMMENT")}
 		pullRequestReview, _, err := githubClient.PullRequests.CreateReview(context.Background(), *owner, *repo, num, pullRequestReviewRequest)
 		if err != nil {
@@ -184,6 +210,7 @@ func main() {
 
 		fmt.Println("github-commenter: created GitHub PR Review comment", pullRequestReview.ID)
 	} else if *commentType == "issue" || *commentType == "pr" {
+		// https://developer.github.com/v3/issues/comments
 		num, err := getPullRequestOrIssueNumber(*number)
 		if err != nil {
 			log.Fatal(err)
@@ -199,7 +226,29 @@ func main() {
 			log.Fatal(err)
 		}
 
-		// https://developer.github.com/v3/issues/comments
+		// Find and delete existing comment(s) before creating the new one
+		if *deleteCommentRegex != "" {
+			r, err := regexp.Compile(*deleteCommentRegex)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			listOptions := &github.IssueListCommentsOptions{}
+			comments, _, err := githubClient.Issues.ListComments(context.Background(), *owner, *repo, num, listOptions)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, comment := range comments {
+				if r.MatchString(*comment.Body) {
+					_, err = githubClient.Issues.DeleteComment(context.Background(), *owner, *repo, int(*comment.ID))
+					if err != nil {
+						log.Print("Error deleting Issue/PR comment", err)
+					}
+				}
+			}
+		}
+
 		issueComment := &github.IssueComment{Body: &formattedComment}
 		issueComment, _, err = githubClient.Issues.CreateComment(context.Background(), *owner, *repo, num, issueComment)
 		if err != nil {
@@ -208,6 +257,7 @@ func main() {
 
 		fmt.Println("github-commenter: created GitHub Issue comment", issueComment.ID)
 	} else if *commentType == "pr-file" {
+		// https://developer.github.com/v3/pulls/comments
 		num, err := getPullRequestOrIssueNumber(*number)
 		if err != nil {
 			log.Fatal(err)
@@ -238,7 +288,29 @@ func main() {
 			log.Fatal(err)
 		}
 
-		// https://developer.github.com/v3/pulls/comments
+		// Find and delete existing comment(s) before creating the new one
+		if *deleteCommentRegex != "" {
+			r, err := regexp.Compile(*deleteCommentRegex)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			listOptions := &github.PullRequestListCommentsOptions{}
+			comments, _, err := githubClient.PullRequests.ListComments(context.Background(), *owner, *repo, num, listOptions)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, comment := range comments {
+				if r.MatchString(*comment.Body) {
+					_, err = githubClient.PullRequests.DeleteComment(context.Background(), *owner, *repo, int(*comment.ID))
+					if err != nil {
+						log.Print("Error deleting Issue/PR comment", err)
+					}
+				}
+			}
+		}
+
 		pullRequestComment := &github.PullRequestComment{Body: &formattedComment, Path: file, Position: &position, CommitID: sha}
 		pullRequestComment, _, err = githubClient.PullRequests.CreateComment(context.Background(), *owner, *repo, num, pullRequestComment)
 		if err != nil {
